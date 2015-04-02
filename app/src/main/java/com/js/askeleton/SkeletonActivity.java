f077a150069449aa3da0ccdde86c2d9257f3fe44
Jeff Sember <jpsember@gmail.com>
2015-04-02 10:35:06 -0700

    Issue #14: deleted MyActivity

--- app/src/main/java/com/js/askeleton/SkeletonActivity.java
+++ app/src/main/java/com/js/askeleton/SkeletonActivity.java
@@ -3,17 +3,19 @@ package com.js.askeleton;
 import static com.js.basic.Tools.*;
 
 import com.js.android.AppPreferences;
-import com.js.android.MyActivity;
 
+import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
-public class SkeletonActivity extends MyActivity {
+public class SkeletonActivity extends Activity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     startApp(this);
+    AppPreferences.prepare(this);
+
     super.onCreate(savedInstanceState);
     View view = prepareView();
     setContentView(view);
@@ -21,8 +23,6 @@ public class SkeletonActivity extends MyActivity {
 
 
   private View prepareView() {
-    AppPreferences.prepare(this);
-
     View view = new View(this);
     {
       int colorIndex = AppPreferences.getUniqueIdentifier("color");
